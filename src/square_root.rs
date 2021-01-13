329b09c70341ba27cf73f55681062ca144f396ab
Christopher Codrington <chcodrington@gmail.com>
2021-01-13 10:44:23 -0500

    Update, implemented clippy suggestion

--- src/square_root.rs
+++ src/square_root.rs
@@ -1,6 +1,6 @@
 pub fn square_root(number: i32) -> Result<i32, &'static str> {
     if number < 0 {
-        return Err("Cannot square root negative numbers")
+        return Err("Cannot square root negative numbers");
     }
 
     let num = number as f32;
@@ -9,7 +9,9 @@ pub fn square_root(number: i32) -> Result<i32, &'static str> {
     loop {
         let next_guess: f32 = 0.5 * (guess + (num / guess));
 
-        if guess.floor() == next_guess.floor() {
+        // this was guidence from clippy, rather than using:
+        // if guess.floor() == next_guess.floor() {
+        if (guess.floor() - next_guess.floor()).abs() < f32::EPSILON {
             return Ok(next_guess as i32);
         }
 
@@ -19,7 +21,7 @@ pub fn square_root(number: i32) -> Result<i32, &'static str> {
 
 #[cfg(test)]
 mod tests {
-    use super:: *;
+    use super::*;
     #[test]
     fn normal_square() {
         assert_eq!(square_root(4).unwrap(), 2);

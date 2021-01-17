94077b916ced88b7083e45116266f6c69adb6ae3
Christopher Codrington <chcodrington@gmail.com>
2021-01-16 22:27:01 -0500

    Update, possible solution to Probelm 1673.
    using a min heap to order the values, I have this concept of a solution
    window. for every new valid number to add to solution vector, I move
    lower bound to new index and increment upper bound by one, which ensures
    that the next valid value is a subsequent one.

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -1,3 +1,7 @@
+use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
+use std::collections::BinaryHeap;
+
+// Problem 1672
 pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
     let mut current_max: i32 = 0;
     for account in accounts.iter() {
@@ -14,6 +18,50 @@ pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
     return current_max;
 }
 
+// Problem 1673
+struct CompetitivePair(i32, usize);
+
+impl Ord for CompetitivePair {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl PartialOrd for CompetitivePair {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(other.cmp(self))
+    }
+}
+
+impl PartialEq for CompetitivePair {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Eq for CompetitivePair {}
+
+pub fn most_competitive(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let mut min_heap = BinaryHeap::with_capacity(nums.len());
+    let mut result: Vec<i32> = Vec::new();
+    let mut lower_bound: usize = 0;
+
+    for (pos, val) in nums.iter().enumerate() {
+        min_heap.push(CompetitivePair(*val, pos));
+    }
+
+    while result.len() < k as usize {
+        let upper_bound: usize = nums.len() - (k as usize - result.len());
+        let next = min_heap.pop().unwrap();
+        if next.1 >= lower_bound && next.1 <= upper_bound {
+            result.push(next.0);
+            lower_bound = next.1;
+        }
+    }
+
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +75,14 @@ mod tests {
 
         let accounts3: Vec<Vec<i32>> = vec![vec![2, 8, 7], vec![7, 1, 3], vec![1, 9, 5]];
         assert_eq!(maximum_wealth(accounts3), 17);
+    }
+
+    #[test]
+    fn check_most_competitive() {
+        let nums: Vec<i32> = vec![3, 5, 2, 6];
+        assert_eq!(most_competitive(nums, 2), vec![2, 6]);
+
+        let nums2: Vec<i32> = vec![2, 4, 3, 3, 5, 4, 9, 6];
+        assert_eq!(most_competitive(nums2, 4), vec![2, 3, 3, 4]);
     }
 }

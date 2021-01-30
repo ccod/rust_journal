60a3d999a6a1e570550561b5682bf9f7852015e0
Christopher Codrington <chcodrington@gmail.com>
2021-01-29 21:13:39 -0500

    Update, leetcode solution to problem 1389

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -203,6 +203,15 @@ impl OrderedStream {
     }
 }
 
+// Problem 1389
+pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::with_capacity(nums.len());
+    for (pos, num) in nums.iter().enumerate() {
+        result.insert(index[pos] as usize, *num);
+    }
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -286,5 +295,11 @@ mod tests {
         assert_eq!(o.insert(2, String::from("bbbbb")), vec!["bbbbb", "ccccc"]);
         assert_eq!(o.insert(5, String::from("eeeee")), a);
         assert_eq!(o.insert(4, String::from("ddddd")), vec!["ddddd", "eeeee"]);
+    }
+
+    #[test]
+    fn check_create_target_array() {
+        let (nums, indexes): (Vec<i32>, Vec<i32>) = (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 2, 1]);
+        assert_eq!(create_target_array(nums, indexes), vec![0, 4, 1, 3, 2]);
     }
 }
